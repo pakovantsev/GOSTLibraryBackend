581d351bd37b76a978380a17b95c8342f4f80d75
Akovancev <apm120700@gmail.com>
2021-10-02 01:31:01 +0300

    Add book model

--- models/book.js
+++ models/book.js
@@ -1,14 +1,54 @@
 const mongoose = require('mongoose')
 
 const book = new mongoose.Schema({
-    author: [ {
+    authors: [ {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'People',
-        required: true
     } ],
     title: {
         type: String,
         required: true
+    },
+    titleInfo: {
+        type: String,
+    },
+    editors: [ {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'People',
+    } ],
+    translators: [ {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'People',
+    } ],
+    collectives: [ {
+        type: String,
+    } ],
+    place: {
+        type: String,
+    },
+    publishingHouse: {
+        type: String,
+    },
+    rePlace: {
+        type: String,
+    },
+    rePublishingHouse: {
+        type: String,
+    },
+    yearOfPublishing: {
+        type: String,
+    },
+    nunberOfPages: {
+        type: Number,
+    },
+    tomNumber: {
+        type: Number,
+    },
+    tomCount: {
+        type: Number,
+    },
+    tomName: {
+        type: String,
     },
 })
 
