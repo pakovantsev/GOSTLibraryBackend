4607b4d13563044b07ce8e1522a8ab58c852f6ea
Akovancev <apm120700@gmail.com>
2021-10-02 20:15:04 +0300

    Add articleNewspaper model

---
+++ models/articleNewspaper.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const articleNewspaper = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    newspaperTitle: {
+        type: String,
+        required: true,
+    },
+    newspaperNumber: {
+        type: Number,
+        required: true,
+    },
+    authors: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'People',
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    numberOfPages: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('ArticleNewspaper', articleNewspaper)
